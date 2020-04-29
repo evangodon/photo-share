783fdac00bec49e4696a723c0f35204b65c9fe30
Evan Godon <e.godon@outlook.com>
2020-04-28 23:05:43 -0400

    Add error message for missing faunadb secret

--- src/lib/faundb.ts
+++ src/lib/faundb.ts
@@ -2,6 +2,10 @@ import got from 'got';
 
 const FAUNA_SECRET = process.env.FAUNADB_SECRET;
 
+if (!FAUNA_SECRET) {
+  throw new Error('Missing fauna_db secret.');
+}
+
 const custom = got.extend({
   prefixUrl: 'https://graphql.fauna.com',
   responseType: 'json',
