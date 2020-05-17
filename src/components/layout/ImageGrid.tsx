1b6b0271faf946b622919d9075064408d2c0338c
Evan Godon <e.godon@outlook.com>
2020-05-17 17:38:55 -0400

    Create ImageGrid component

---
+++ src/components/layout/ImageGrid.tsx
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+import Link from 'next/link';
+import { Image } from '@/components';
+
+type Props = {
+  photos: any[];
+  editable?: boolean;
+};
+
+const photosWithoutLinks = (photo) => (
+  <div>
+    <Image
+      key={photo._id}
+      src={photo.url ?? `https://source.unsplash.com/random`}
+    />
+  </div>
+);
+
+const photosWithLinks = (photo) => (
+  <Link href="/photo/photo-slug" key={photo._id}>
+    <a>
+      <Image src={photo.url ?? `https://source.unsplash.com/random`} />
+    </a>
+  </Link>
+);
+
+const ImageGrid = ({ photos, editable }: Props) => {
+  return (
+    <ImageContainer>
+      <>
+        {photos.map(editable ? photosWithLinks : photosWithoutLinks)}
+        <a className="last-row" />
+      </>
+    </ImageContainer>
+  );
+};
+
+const ImageContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-content: stretch;
+
+  a,
+  div {
+    height: 40vh;
+    display: inline-flex;
+    margin: 2px;
+    flex-grow: 1;
+  }
+
+  .last-row {
+    flex-grow: 10;
+  }
+`;
+
+export default ImageGrid;
