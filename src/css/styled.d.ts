15356c4e0d52dfe5cfeee3d240c62a249415a42a
Evan Godon <e.godon@outlook.com>
2020-08-09 00:13:43 -0400

    Update interface of DefaultTheme

--- src/css/styled.d.ts
+++ src/css/styled.d.ts
@@ -1,7 +1,12 @@
 import 'styled-components';
-import { colors, fontSizes, spaces } from './theme';
+import { colors, fontSizes, spaces, shadows } from './theme';
 
-type Theme = { colors: typeof colors; space: typeof spaces; fontSize: typeof fontSizes };
+type Theme = {
+  colors: typeof colors;
+  space: typeof spaces;
+  fontSize: typeof fontSizes;
+  shadows: typeof shadows;
+};
 
 declare module 'styled-components' {
   export interface DefaultTheme extends Theme {}
